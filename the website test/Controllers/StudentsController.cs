bef8978759dd6fa42b5bca02c8d4fd357c2d34fa
MohameOsamaElnzamy <59491250+MohamedOsamaElnzamy@users.noreply.github.com>
2020-08-08 12:55:05 +0200

    website

---
+++ the website test/Controllers/StudentsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using the_website_test.Models;
+
+namespace the_website_test.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private DB_A50C7A_FEEEntities db = new DB_A50C7A_FEEEntities();
+
+        // GET: Students
+        public ActionResult Index()
+        {
+            var students = db.Students.Include(s => s.Departments);
+            return View(students.ToList());
+        }
+
+        // GET: Students/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Students students = db.Students.Find(id);
+            if (students == null)
+            {
+                return HttpNotFound();
+            }
+            return View(students);
+        }
+
+        // GET: Students/Create
+        public ActionResult Create()
+        {
+            ViewBag.DeptID = new SelectList(db.Departments, "ID", "DeptName");
+            return View();
+        }
+
+        // POST: Students/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,SSN,AcademicNumber,FullName,City,FullAddress,Gender,CreatedAt,DateOfBirth,DeptID")] Students students)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Students.Add(students);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.DeptID = new SelectList(db.Departments, "ID", "DeptName", students.DeptID);
+            return View(students);
+        }
+
+        // GET: Students/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Students students = db.Students.Find(id);
+            if (students == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.DeptID = new SelectList(db.Departments, "ID", "DeptName", students.DeptID);
+            return View(students);
+        }
+
+        // POST: Students/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,SSN,AcademicNumber,FullName,City,FullAddress,Gender,CreatedAt,DateOfBirth,DeptID")] Students students)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(students).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DeptID = new SelectList(db.Departments, "ID", "DeptName", students.DeptID);
+            return View(students);
+        }
+
+        // GET: Students/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Students students = db.Students.Find(id);
+            if (students == null)
+            {
+                return HttpNotFound();
+            }
+            return View(students);
+        }
+
+        // POST: Students/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Students students = db.Students.Find(id);
+            db.Students.Remove(students);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ the website test/Controllers/SubjectsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using the_website_test.Models;
+
+namespace the_website_test.Controllers
+{
+    public class SubjectsController : Controller
+    {
+        private DB_A50C7A_FEEEntities db = new DB_A50C7A_FEEEntities();
+
+        // GET: Subjects
+        public ActionResult Index()
+        {
+            var subjects = db.Subjects.Include(s => s.Departments);
+            return View(subjects.ToList());
+        }
+
+        // GET: Subjects/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Subjects subjects = db.Subjects.Find(id);
+            if (subjects == null)
+            {
+                return HttpNotFound();
+            }
+            return View(subjects);
+        }
+
+        // GET: Subjects/Create
+        public ActionResult Create()
+        {
+            ViewBag.DeptID = new SelectList(db.Departments, "ID", "DeptName");
+            return View();
+        }
+
+        // POST: Subjects/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,SubjectName,TotalMarks,Code,NHours,DeptID")] Subjects subjects)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Subjects.Add(subjects);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.DeptID = new SelectList(db.Departments, "ID", "DeptName", subjects.DeptID);
+            return View(subjects);
+        }
+
+        // GET: Subjects/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Subjects subjects = db.Subjects.Find(id);
+            if (subjects == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.DeptID = new SelectList(db.Departments, "ID", "DeptName", subjects.DeptID);
+            return View(subjects);
+        }
+
+        // POST: Subjects/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,SubjectName,TotalMarks,Code,NHours,DeptID")] Subjects subjects)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(subjects).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DeptID = new SelectList(db.Departments, "ID", "DeptName", subjects.DeptID);
+            return View(subjects);
+        }
+
+        // GET: Subjects/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Subjects subjects = db.Subjects.Find(id);
+            if (subjects == null)
+            {
+                return HttpNotFound();
+            }
+            return View(subjects);
+        }
+
+        // POST: Subjects/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Subjects subjects = db.Subjects.Find(id);
+            db.Subjects.Remove(subjects);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
